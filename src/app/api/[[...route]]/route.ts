d4d3109d13a06101db0048fafa7b832ecdf2d389
themujahidkhan <mujucoder@gmail.com>
2024-08-04 23:12:56 +0530

    fixed cors problem

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -3,6 +3,7 @@ import { Context, Hono } from "hono";
 
 import ai from "./ai";
 import authConfig from "@/auth.config";
+import { cors } from "hono/cors";
 import { handle } from "hono/vercel";
 import images from "./images";
 import projects from "./projects";
@@ -13,22 +14,32 @@ import users from "./users";
 export const runtime = "nodejs";
 
 function getAuthConfig(c: Context): AuthConfig {
-  return {
-    secret: c.env.AUTH_SECRET,
-    ...authConfig
-  };
-};
+	return {
+		secret: c.env.AUTH_SECRET,
+		...authConfig,
+	};
+}
 
 const app = new Hono().basePath("/api");
 
-app.use("*", initAuthConfig(getAuthConfig));
+app.use(
+	"*",
+	cors({
+		origin: ["https://app.graphicai.design", "http://localhost:3000"],
+		allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+		allowHeaders: ["Content-Type", "Authorization"],
+		credentials: true,
+	}),
+);
 
 const routes = app
-  .route("/ai", ai)
-  .route("/users", users)
-  .route("/images", images)
-  .route("/projects", projects)
-  .route("/subscriptions", subscriptions);
+	.route("/ai", ai)
+	.route("/users", users)
+	.route("/images", images)
+	.route("/projects", projects)
+	.route("/subscriptions", subscriptions);
+
+app.use("*", initAuthConfig(getAuthConfig));
 
 export const GET = handle(app);
 export const POST = handle(app);
