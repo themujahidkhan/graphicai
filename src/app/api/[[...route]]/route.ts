861a15bdc5c2663b02c5399056fe6ccbcca153c7
themujahidkhan <mujucoder@gmail.com>
2024-08-05 09:26:27 +0530

    fix: app is not a valid Route export field

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,53 +1,47 @@
-import { AuthConfig, initAuthConfig } from "@hono/auth-js";
-import { Context, Hono } from "hono";
+import { Context, Env } from "hono";
 
-import ai from "./ai";
+import { Hono } from "hono";
+import ai from "@/api/ai";
 import authConfig from "@/auth.config";
 import { cors } from "hono/cors";
 import { handle } from "hono/vercel";
-import images from "./images";
-import projects from "./projects";
-import subscriptions from "./subscriptions";
-import users from "./users";
+import images from "@/api/images";
+import projects from "@/api/projects";
+import subscriptions from "@/api/subscriptions";
+import users from "@/api/users";
 
-// Revert to "edge" if planning on running on the edge
-export const runtime = "nodejs";
-
-function getAuthConfig(c: Context): AuthConfig {
+// Function to get AuthConfig
+function getAuthConfig(c: Context<Env>): AuthConfig {
 	return {
 		secret: c.env.AUTH_SECRET,
 		...authConfig,
 	};
 }
 
+// Initialize Hono App
 const app = new Hono().basePath("/api");
 
 // Configure CORS
 app.use(
 	"*",
 	cors({
-		origin: [
-			"https://app.graphicai.design",
-			"http://localhost:3000",
-			"https://graphicai.design",
-		],
-		allowMethods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
-		allowHeaders: ["Content-Type", "Authorization"],
+		origin: "https://app.graphicai.design",
+		methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+		allowedHeaders: ["Content-Type", "Authorization"],
 		credentials: true,
 	}),
 );
 
+// Apply authentication configuration
 app.use("*", initAuthConfig(getAuthConfig));
 
-// Routes
-const routes = app
+// Define routes
+app
 	.route("/ai", ai)
 	.route("/users", users)
 	.route("/images", images)
 	.route("/projects", projects)
 	.route("/subscriptions", subscriptions);
-
-app.route("/api", routes);
 
 // Handle all HTTP methods
 export const GET = handle(app);
@@ -56,5 +50,22 @@ export const PATCH = handle(app);
 export const DELETE = handle(app);
 export const OPTIONS = handle(app);
 
-// Export the app
-export default app;
+// Handle OPTIONS requests specifically for CORS
+app.options("*", (c) => {
+	c.res.headers.set(
+		"Access-Control-Allow-Origin",
+		"https://app.graphicai.design",
+	);
+	c.res.headers.set(
+		"Access-Control-Allow-Methods",
+		"GET, POST, PATCH, DELETE, OPTIONS",
+	);
+	c.res.headers.set(
+		"Access-Control-Allow-Headers",
+		"Content-Type, Authorization",
+	);
+	c.res.headers.set("Access-Control-Allow-Credentials", "true");
+	return c.text("OK");
+});
+
+export default handle(app);
