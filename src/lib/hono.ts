0dc2fd9bfdf171a200637663f27e56ef80434c9f
themujahidkhan <mujucoder@gmail.com>
2024-08-05 11:03:10 +0530

    fix: Add RequestInit type to fetch init parameter

--- src/lib/hono.ts
+++ src/lib/hono.ts
@@ -2,7 +2,7 @@ import { AppType } from "@/app/api/[[...route]]/route";
 import { hc } from "hono/client";
 
 export const client = hc<AppType>(process.env.NEXT_PUBLIC_APP_URL || "", {
-	fetch: (input, init) => {
+	fetch: (input: RequestInfo | URL, init?: RequestInit) => {
 		return fetch(input, {
 			...init,
 			credentials: "include",
