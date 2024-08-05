791e3e674cc70a109b07a01fa32dfd523f4c60c5
themujahidkhan <mujucoder@gmail.com>
2024-08-05 11:35:29 +0530

    fixed banner.tsx file

--- src/app/(dashboard)/banner.tsx
+++ src/app/(dashboard)/banner.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { ArrowRight, Sparkles } from "lucide-react";
+
 import { Button } from "@/components/ui/button";
 import { useCreateProject } from "@/features/projects/api/use-create-project";
 import { useRouter } from "next/navigation";
@@ -8,38 +10,18 @@ export const Banner = () => {
 	const router = useRouter();
 	const mutation = useCreateProject();
 
-	// @ts-ignore
-	const handleSelect = (type) => {
-		createProject(type);
-	};
-
-	// @ts-ignore
-	const createProject = (type) => {
+	const onClick = () => {
 		mutation.mutate(
+			// @ts-ignore
 			{
-				name: `Untitled ${type.label}`,
+				name: "Untitled project",
 				json: "",
-				width: type.width,
-				height: type.height,
+				width: 900,
+				height: 1200,
 			},
 			{
 				onSuccess: ({ data }) => {
-					router.push(`/editor/${data.id}`);
-				},
-			},
-		);
-	};
-
-	const createBlankProject = () => {
-		mutation.mutate(
-			{
-				name: "Untitled Project",
-				json: "",
-				width: 1080,
-				height: 1080,
-			},
-			{
-				onSuccess: ({ data }) => {
+					// @ts-ignore
 					router.push(`/editor/${data.id}`);
 				},
 			},
@@ -47,23 +29,28 @@ export const Banner = () => {
 	};
 
 	return (
-		<div className="text-black aspect-[5/1] min-h-[248px] flex flex-col p-6 rounded-xl bg-gradient-to-r from-purple-500 to-pink-100 relative">
-
-			<div className="flex flex-col gap-y-4 items-center justify-center flex-grow">
-				<h2 className="text-xl md:text-3xl font-semibold text-center">
-					Bring your creative vision to life with AI-powered graphics
-				</h2>
-				<p className="text-xs md:text-sm text-center">
+		<div className="text-black aspect-[5/1] min-h-[248px] flex gap-x-6 p-6 items-center rounded-xl bg-gradient-to-r from-[#e1ed05] to-[#fbfbfb]">
+			<div className="rounded-full size-28 items-center justify-center bg-white/50 hidden md:flex">
+				<div className="rounded-full size-20 flex items-center justify-center bg-white">
+					<Sparkles className="h-20 text-[#0073ff] fill-[#0073ff]" />
+				</div>
+			</div>
+			<div className="flex flex-col gap-y-2">
+				<h1 className="text-xl md:text-3xl font-semibold">
+					Visualize your ideas with Graphic AI
+				</h1>
+				<p className="text-xs md:text-sm mb-2">
 					Build your online presence with AI Graphic Design
 				</p>
-				<Button onClick={createBlankProject} variant="secondary">
-					Start Blank
+				<Button
+					disabled={mutation.isPending}
+					onClick={onClick}
+					variant="secondary"
+					className="w-[160px]"
+				>
+					Start creating
+					<ArrowRight className="size-4 ml-2" />
 				</Button>
-				{mutation.isPending && (
-					<p className="text-sm text-muted-foreground">
-						Creating your project...
-					</p>
-				)}
 			</div>
 		</div>
 	);
