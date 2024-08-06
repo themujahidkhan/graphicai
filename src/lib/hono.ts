901ad333082c2103385b560d458d1369c22a0829
themujahidkhan <mujucoder@gmail.com>
2024-08-06 10:16:44 +0530

    modified api url

--- src/lib/hono.ts
+++ src/lib/hono.ts
@@ -1,6 +1,7 @@
 import { hc } from "hono/client";
 
-const apiUrl = process.env.NEXT_PUBLIC_API_URL || "https://graphicai.design";
+const apiUrl =
+	process.env.NEXT_PUBLIC_API_URL || "https://app.graphicai.design";
 
 export const client = hc(apiUrl, {
 	headers: {
